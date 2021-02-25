f00cb5fd33a956ee4b8175c2bc3e99396d8b86c8
allen <294931812@qq.com>
2021-02-25 11:17:48 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Son = ({ onClick }) => <div onClick={onClick}>Son</div>;
+
+const Child = ({ onClick }) => (
+  <div>
+    <Son onClick={onClick} />
+  </div>
+);
+
+class App extends React.Component {
+  componentDidMount() {}
+
+  click = e => {
+    console.log(e, this);
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Child onClick={this.click} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/reactDomMini.js
@@ -0,0 +1,75 @@
+import ReactReconciler from 'react-reconciler';
+
+let reconciler = ReactReconciler({
+  supportsMutation: true,
+
+  now() {
+    return Date.now();
+  },
+
+  getPublicInstance() {},
+
+  getRootHostContext() {},
+
+  getChildHostContext(parentHostContext, type, rootContainerInstance) {
+    return parentHostContext;
+  },
+
+  shouldSetTextContent() {
+    return false;
+  },
+
+  createInstance(type, props, rootContainer, hostContext, internalHandle) {
+    console.log(type, props, rootContainer, 'instance');
+  },
+
+  createTextInstance(text, rootContainer, hostContext, internalHandle) {
+    console.log(text, rootContainer, hostContext, internalHandle);
+  },
+
+  prepareForCommit() {},
+
+  resetAfterCommit() {},
+
+  commitTextUpdate() {},
+
+  prepareUpdate() {},
+
+  commitUpdate() {},
+
+  appendInitialChild() {},
+
+  appendChild() {},
+
+  clearContainer() {},
+
+  insertBefore() {},
+
+  removeChild() {},
+
+  finalizeInitialChildren() {},
+
+  appendChildToContainer() {},
+
+  insertInContainerBefore() {},
+
+  removeChildFromContainer() {},
+
+  hideInstance() {},
+
+  hideTextInstance() {},
+
+  unhideInstance() {},
+
+  unhideTextInstance() {}
+});
+
+const render = (element, container) => {
+  let _container = reconciler.createContainer(container, false, false);
+
+  reconciler.updateContainer(element, _container, null, null);
+};
+
+export default {
+  render
+};
